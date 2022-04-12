a0e7e27ec57c02b3a0dca2abdf77c2f93f4d2a03
larbibaraka <larbibaraka@gmail.com>
2022-04-13 00:41:57 +0100

    create books Dto

---
+++ src/books/book.dto.ts
@@ -0,0 +1,14 @@
+export class BookDto  {
+
+    isbn : String;
+    price : number;
+    title : String;
+    subtitle : String;
+    author : String;
+    published : Date;
+    publisher : String;
+    pages : number;
+    description : String;
+    website : String;
+
+}
