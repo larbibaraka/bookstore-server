faf90b6014aabd12d703b872b62a2b77b99c3bc3
larbibaraka <larbibaraka@gmail.com>
2022-04-12 01:42:10 +0100

    Install elasticSearch and register it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+ import { BooksModule } from './books/books.module';
+import { BooksController } from './books/books.controller';
+import { BooksService } from './books/books.service';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+ 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), BooksModule],
+  controllers: [AppController, BooksController],
+  providers: [AppService, BooksService],
 })
 export class AppModule {}
