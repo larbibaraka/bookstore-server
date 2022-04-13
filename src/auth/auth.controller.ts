d2e4ba36a4663c0ef19f168425790ffd0331af28
larbibaraka <larbibaraka@gmail.com>
2022-04-14 00:04:46 +0100

    create login using google auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+ 
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  googleLogin() {
+    // initiates the Google OAuth2 login flow
+  }
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleLoginCallback(@Req() req, @Res() res) {
+    // handles the Google OAuth2 callback
+    const jwt = req.user.jwt;
+    res.redirect('http://localhost:3000/login/succes/' + jwt);
+  }
+
+}
+
