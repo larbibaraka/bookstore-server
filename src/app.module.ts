9dcbeff5d1306e851f9d106eed85dab11a307fdb
larbibaraka <larbibaraka@gmail.com>
2022-04-14 00:41:14 +0100

    clean app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { BooksModule } from './books/books.module';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
 import { AuthModule } from './auth/auth.module';
-
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), BooksModule, AuthModule],
   controllers: [BooksController],
