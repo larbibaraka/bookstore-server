fbd25a4034bcc50fd8f555d2e2c3cfe062bfc2d4
larbibaraka <larbibaraka@gmail.com>
2022-04-14 00:04:15 +0100

    create google oauth strategy

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(config: ConfigService) {
+    super({
+      clientID: config.get('GOOGLE_CLIENT_ID'),
+      clientSecret: config.get('GOOGLE_CLIENT_SECRET'),
+      callbackURL: config.get('GOOGLE_callbackURL'),
+      passReqToCallback: true,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    request: any,
+    accessToken: string,
+    refreshToken: string,
+    profile,
+    done: VerifyCallback,
+  ) {
+    try {
+      console.log(profile);
+
+      const jwt = 'jsjsjs';
+
+      const user = {
+        jwt,
+      };
+
+      done(null, user);
+    } catch (err) {
+      // console.log(err)
+      done(err, false);
+    }
+  }
+}
