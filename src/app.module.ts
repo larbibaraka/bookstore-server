ba512e464a47337a66880779beca36f4c3ae1b16
larbibaraka <larbibaraka@gmail.com>
2022-04-13 11:57:03 +0100

    Install saml & hook auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), BooksModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), BooksModule, AuthModule],
   controllers: [BooksController],
   providers: [BooksService],
 })
