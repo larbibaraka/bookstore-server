21fefe47dc3a2db0b09f8d67b87679fb4e10473d
larbibaraka <larbibaraka@gmail.com>
2022-04-16 02:26:26 +0100

    adding Port from .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors : true});
-  await app.listen(3001);
+  const logger = new Logger('Bootstrapper');
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const config = app.get<ConfigService>(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  const port = config.get('SERVER_PORT');
+  await app.listen(Number(port));
+  logger.log(`App listening on port ${port}`);
 }
 bootstrap();
