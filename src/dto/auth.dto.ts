48dee1c9dccb770008265466341a7b3a87717a26
larbibaraka <larbibaraka@gmail.com>
2022-04-16 02:27:59 +0100

    refactore dto

--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -1,14 +1,20 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 export class AuthDto {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   password: string;
 
   @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   firstName: string;
 
   @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   lastName: string;
 }
